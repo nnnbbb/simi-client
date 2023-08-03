f12111b41f0539967db806f7190ad60ea97ef873
yanxl <923932093@qq.com>
2023-08-03 15:32:33 +0800

    refactor(*): 重构首页录入单词输入框

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,59 +1,48 @@
-import { Button, Layout, message, Select } from 'antd';
+import { AutoComplete, Button, message } from 'antd';
 import { useState } from 'react';
 import { wordCreate } from '../../services/word/word-create.service';
 import { Http } from '../../utils/Http';
 import styles from './styles.module.css';
 
-const { Header, Content, Footer } = Layout;
-
 export default function Home() {
   const [suggest, setSuggest] = useState<any[]>([]);
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
 
   async function onSearch(word: string) {
-    let res = await Http.get(`/suggest?word=${word}`)
-    setSuggest(res.data.entries)
+    let res = await Http.get(`/suggest?word=${word}`);
+    setSuggest(res.data.entries);
   }
 
   const handleConfirm = async () => {
-    let res = await wordCreate({ word })
-    message.success("添加成功", 1)
+    let res = await wordCreate({ word });
+    message.success('添加成功', 1);
     // notification.success({ message: "添加成功" });
-    setWord("")
+    setWord('');
   };
 
   return (
     <main className={styles.main}>
-      <div className={styles.container} >
-        <Select
-          size='large'
+      <div className={styles.container}>
+        <AutoComplete
+          size="large"
           showSearch
           value={word}
-          optionLabelProp={word}
-          // onBlur={(e) => {
-          //   const target = e.target as HTMLTextAreaElement;
-          //   setWord(target.value)
-          // }}
-          style={{ width: "80%", marginRight: 8 }}
+          style={{ width: '80%', marginRight: 8 }}
           placeholder="在此输入单词或文本"
-          onSearch={(word) => {
-            onSearch(word)
-          }}
+          onSearch={(word) => onSearch(word)}
           onChange={(word) => setWord(word)}
-
           showArrow={false}
-          options={suggest?.map(it => {
-            return ({
+          options={suggest?.map((it) => {
+            return {
               value: it.entry,
               label: `${it.entry} ${it.explain}`,
-            })
+            };
           })}
-        >
-        </Select>
-        <Button type="primary" size='middle' style={{}} onClick={handleConfirm}>
+        ></AutoComplete>
+        <Button type="primary" size="middle" style={{}} onClick={handleConfirm}>
           添加
         </Button>
       </div>
     </main>
-  )
+  );
 }
